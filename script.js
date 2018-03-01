342d3aae9bb3e3c315731a3a1af3d509bcf6e743
John Milner <john.milner@mac.com>
2018-02-28 23:09:39 -0800

    add ES6 project code

--- script.js
+++ script.js
@@ -624,15 +624,68 @@ It's a very small town, so right now there are only 3 parks and 4 streets. All p
 At an end-of-year meeting, your boss wants a final report with the following:
 1. Tree density of each park in the town (forumla: number of trees/park area)
 2. Average age of each town's park (forumla: sum of all ages/number of parks)
+    //create two objects
 3. The name of the park that has more than 1000 trees
+    //use map
 4. Total and average length of the town's streets
 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+    //default parameters
 
 All the report data should be printed to the console.
 
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
+
+class Element {
+    constructor(name, buildYear) {
+        this.name = name;
+        this.buildYear = buildYear;
+    }
+}
+
+class Park extends Element {
+    constructor(name, buildYear, area, numTrees) {
+        super(name, buildYear) {
+            this.area = area;
+            this.numTrees = numTrees;
+        }
+    }
+
+    treeDensity() {
+        const density = this.area / this.numTrees;
+        console.log(`${this.name} has a tree density of ${density} trees per square kilometer.`);
+    }
+}
+
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear) {
+            this.length = length;
+            this.size = size;
+        }
+    }
+
+    classifyStreet() {
+        const classification = new Map;
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'medium');
+        classification.set(4, 'large');
+        classification.set(5, 'huge');
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+    }
+}
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215),
+                 new Park('National Park', 1894, 2.9, 3541),
+                 new Park('Oak Park', 1953, 0.4, 949)];
+
+const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
+                   new Street('Evergreen Street', 2008, 2.7, 2),
+                   new Street('4th Street', 2015, 0.8),
+                   new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
 
 
 class Element {
